d593cd9d66b40df52856c5f317b1ac86095fd41f
David Krauss <david.krauss@allscripts.com.com>
2011-05-10 11:17:29 -0400

    Committing

--- source/nothinbutdotnetprep/CodeKata/PrettyDumpFolder.cs
+++ source/nothinbutdotnetprep/CodeKata/PrettyDumpFolder.cs
@@ -9,61 +9,30 @@ namespace nothinbutdotnetprep.CodeKata
     public class PrettyDumpFolder
     {
 
-        public PrettyDumpFolder(String path)
-        {
-            TraverseTree(path);
-
-        }
-
-
-
         public static void TraverseTree(string root)
         {
-            Stack<string> dirs = new Stack<string>(20);
             if (!Directory.Exists(root))
             {
                 throw new DirectoryNotFoundException(root + " invalid");
             }
-            dirs.Push(root);
-            int depth = 0;
-            while (dirs.Count > 0)
+            var directories = new Stack<string>();
+            directories.Push(root);
+            var depth = 0;
+            while (directories.Count > 0)
             {
-                depth++;
-                var currentDir = dirs.Pop();
-                Console.WriteLine(" ".PadLeft(depth) + "{0}", currentDir);
-                string[] subDirs;
-                try
-                {
-                    subDirs = System.IO.Directory.GetDirectories(currentDir);
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
-
-                string[] files = null;
-                try
-                {
-                    files = Directory.GetFiles(currentDir);
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
+                depth+=2;
+                var currentDir = directories.Pop();
+                Console.WriteLine(" ".PadLeft(depth) + currentDir);
+                var subDirs = Directory.GetDirectories(currentDir);
+                string[] files = files = Directory.GetFiles(currentDir);
 
                 foreach (var file in files)
                 {
-                    try
-                    {
-                        Console.WriteLine(" ".PadLeft(depth) + "{0}", file);
-                    }
-                    catch (Exception)
-                    {
-                    }
+                     Console.WriteLine(" ".PadLeft(depth) + file);
                 }
 
                 foreach (var str in subDirs)
-                    dirs.Push(str);
+                    directories.Push(str);
             }
         }
 
